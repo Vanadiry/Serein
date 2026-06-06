b61f03dcc6c353f927b7b73c5e8a1132b201efbb
Vanadiry <77710715+Vanadiry@users.noreply.github.com>
2026-06-06 15:11:36 +0800

    refactor: 将API文档移出构建包

--- scripts/minify-html.js
+++ scripts/minify-html.js
@@ -7,8 +7,7 @@ const DEST_DIR = path.join(__dirname, '../web');
 
 const FILES = [
     'index.html',
-    'rules.html',
-    'api/index.html'
+    'rules.html'
 ];
 
 async function processFile(file) {
